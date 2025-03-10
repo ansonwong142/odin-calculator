0d8638e000271b6f220188483946fc6a74f90c1f
Anson <ansonwong142@gmail.com>
2025-03-09 20:24:27 -0400

    add event listener and functions for numbers and operator

---
+++ javascript.js
@@ -0,0 +1,62 @@
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+
+const currentDisplayNumber = document.querySelector(".currentNumber");
+const previousDisplayNumber = document.querySelector(".previousNumber");
+
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector(".equal");
+
+equalButton.addEventListener("click", () => operate());
+
+numberButtons.forEach(button =>
+    button.addEventListener("click", e => processNumber(e.target.textContent))
+);
+
+function processNumber(number) {
+    currentNumber += number;
+    currentDisplayNumber.textContent = currentNumber;
+}
+
+operatorButtons.forEach(button =>
+    button.addEventListener("click", e => processOperator(e.target.textContent))
+);
+
+function processOperator(pressedOperator) {
+    operator = pressedOperator;
+    previousNumber = currentNumber;
+    currentNumber = "";
+    previousDisplayNumber.textContent = previousNumber + " " + operator;
+    currentDisplayNumber.textContent = "0";
+}
+
+function operate() {
+    previousNumber = Number(previousNumber);
+    currentNumber = Number(currentNumber);
+
+    if (operator === "+") {
+        previousNumber = add(previousNumber, currentNumber);
+    }
+
+}
+
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
